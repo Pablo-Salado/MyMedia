d3c4bceb0bf62981be98d3171a9281eeefbbd8fd
Galoan <galoan2001@uma.es>
2021-05-29 22:05:20 +0200

    Added run class to unify GUI's

---
+++ src/main/java/Run.java
@@ -0,0 +1,18 @@
+import db.User;
+import forogui.Frame_Foro;
+import logingui.LoginGUI;
+
+public class Run {
+    public static void main(String[] args) {
+        User currentUser;
+        LoginGUI loginGUI = new LoginGUI();
+        Frame_Foro frame_foro = new Frame_Foro();
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                loginGUI.setVisible(true);
+            }
+        });
+    }
+}
