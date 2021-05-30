1574830186095fd37ff3cd239139abb8a6d89e2e
AndresGL01 <andreslp0001@gmail.com>
2021-05-30 16:28:01 +0200

    Depurated run thread

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -9,34 +9,16 @@ import java.util.concurrent.locks.ReentrantLock;
 
 
 public class Run {
-    private static class threadChecker extends Thread{
-        LoginGUI loginGUI;
-        private boolean isLogued = false;
 
-        private threadChecker(LoginGUI loginGUI){this.loginGUI = loginGUI;}
-
-        public void run(){
-            while (!loginGUI.logued()){
-                this.yield();
-            };
-            isLogued = true;
-        }
-
-        public boolean isLogued() {
-            return isLogued;
-        }
-    }
     public static void main(String[] args) throws InterruptedException {
 
         LoginGUI loginGUI = new LoginGUI();
         Frame_Foro frame_foro = new Frame_Foro();
         DBConnection DBAccess = DBConnectionJDBC.getInstance();
         peliculasGUI peliculasGUI = new peliculasGUI();
-        threadChecker threadChecker = new threadChecker(loginGUI);
 
         loginGUI.setVisible(true);
-        threadChecker.start();
-        while (!threadChecker.isLogued()){
+        while (!loginGUI.logued()){
             Thread.yield();
         }
         loginGUI.setVisible(false);
